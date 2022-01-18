16ace128d39c0772a04791c91be754bd48af34d9
Louis Dutton <clouisdutton@gmail.com>
2022-01-18 20:19:25 +0000

    added progress bar

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,20 +1,25 @@
 import Palette from './Palette';
 import List from './List';
 import IconButton from './IconButton';
-import { PenTool, EraserTool, FillTool } from '../lib/tools';
-import { Tool } from '../lib/useCanvas';
-// import { Pen, Eraser, PaintBucket, Rectangle, IconProps } from 'phosphor-react';
-import { TiPen, TiBrush, TiPipette } from 'react-icons/ti';
+import { Color, Tool } from '../lib/useCanvas';
+import { TiPencil, TiBrush, TiPipette } from 'react-icons/ti';
 import { BiEraser, BiDroplet } from 'react-icons/bi';
 import { IconType } from 'react-icons';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
 	activeTool: Tool;
 	setTool: (tool: Tool) => void;
 	setWeight: (value: number) => void;
-	setColor: (value: string) => void;
+	setColor: (value: Color) => void;
 	setOpacity: (value: number) => void;
 }
+
+const lerp = (a: number, b: number, t: number) => {
+	return a * (1 - t) + b * t;
+};
+
+let progress = 0;
 
 const Toolbar = ({
 	setTool,
@@ -24,20 +29,39 @@ const Toolbar = ({
 	activeTool
 }: Props) => {
 	// const [tool, setTool] = useState<Tool>(Tool.Pen);
-	const toolIcons = [TiPen, BiEraser, BiDroplet, TiPipette];
+	const toolIcons = [TiPencil, TiBrush, BiEraser, BiDroplet, TiPipette];
+	// dummy timer
+	const [time, setTime] = useState(60);
+
+	useEffect(() => {
+		setInterval(() => {
+			progress = lerp(progress, (time - 1) / 60, 0.1);
+			setTime((time) => time - 1);
+		}, 100);
+	}, []);
 
 	return (
 		<div className="z-50 bg-neutral-100 dark:bg-neutral-800 absolute bottom-0 left-0 w-screen">
-			<div className="py-3 flex sm:flex-row gap-4 items-center justify-evenly">
+			<div className="w-full h-2 bg-neutral-300 border-b border-neutral-300">
+				<div
+					className="bg-green-400 h-2 timer origin-left"
+					style={{ animationDuration: 60 + 's' }}
+				/>
+			</div>
+			<div className="py-3 flex sm:flex-row gap-4 items-center justify-evenly origin-left">
 				<Palette setColor={(col) => setColor(col)} />
 				<List<IconType>
 					items={toolIcons}
 					render={(Tool, i) => (
-						<IconButton onClick={() => setTool(i)} active={activeTool === i}>
-							<Tool size={24} />
-						</IconButton>
+						<button
+							onClick={() => setTool(i)}
+							className={`flex ${
+								activeTool === i ? 'text-black' : 'text-neutral-400'
+							}`}>
+							<Tool size={30} />
+						</button>
 					)}
-					className="flex gap-2"
+					className="flex gap-3"
 				/>
 				<input
 					type="range"
